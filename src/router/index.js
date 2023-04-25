d8a63f7035d5b0433c742d760b965fce4555eb7e
zhuli <zhuli2@rainbow.com>
2023-04-25 23:21:26 +0800

    fix:默认页面

--- src/router/index.js
+++ src/router/index.js
@@ -8,9 +8,12 @@ Vue.use(VueRouter)
 
 const routes = [
   {
-    path: '/page-b',
-    name: 'PageB',
-    component: () => import(/* webpackChunkName: "about" */ '../views/PageB.vue')
+    path: '/',
+    name: 'PageA',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/PageA.vue')
   },
   {
     path: '/page-a',
@@ -19,6 +22,11 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/PageA.vue')
+  },
+  {
+    path: '/page-b',
+    name: 'PageB',
+    component: () => import(/* webpackChunkName: "about" */ '../views/PageB.vue')
   },
   {
     path: '/page-c',
